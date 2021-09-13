7c5bf8aa03734661001d961878a078c8280bdb08
kisseh <abinetgebire9@gmail.com>
2021-09-13 10:56:05 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,18 @@
-  
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * *_strstr - locates a substring
- * @haystack: string to search in
- * @needle: substring to look for
+ * main - check the code for ALX School students.
  *
- * Return: pointer to the beginning of the located substring
- * or NULL if the substring is not found
+ * Return: Always 0.
  */
-char *_strstr(char *haystack, char *needle)
+int main(void)
 {
-	int i, j;
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
-		if (!needle[j])
-			return (&haystack[i]);
-	}
-	return (NULL);
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
 }
